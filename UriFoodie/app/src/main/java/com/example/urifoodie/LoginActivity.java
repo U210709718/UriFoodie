b21fd113c3afc41d6072222943e7ef08ecda08ad
hkselvi <155230226+hkselvi@users.noreply.git>
2025-01-03 00:42:50 +0300

    it is a try..

--- UriFoodie/app/src/main/java/com/example/urifoodie/LoginActivity.java
+++ UriFoodie/app/src/main/java/com/example/urifoodie/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.urifoodie;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
